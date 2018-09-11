f5cae2ad37c00ac5ab62dec8e40e4a748a78beaf
Janidu Jayasanka <johnnyluther96@gmail.com>
2018-09-11 22:36:28 +0530

    add data types

---
+++ 02 Data types/data types.py
@@ -0,0 +1,17 @@
+a = 10 #this is the interger data type
+print ('this is a integer value {}' .format(a))
+
+b = 'A' #this is the characteristic  data type
+print('this is charater type : {}' .format(b))
+
+c = "this is the string value in python"
+print("string value in python:\n {}".format(c))
+
+d = 200.225
+e = 2.25e5
+i = 2.25e-3
+#type (d)
+print("these are the float values {} , {} , {} ".format(d , e , i))
+
+f = (2+7j)
+print('this is complex data type : {}' .format(f))
